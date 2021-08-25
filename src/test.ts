24124dc1b26b2c06ff127ce85c7230b6f94e038b
Larry Joy <ljoy@microsoft.com>
2021-08-25 15:06:26 -0700

    update

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,1 @@
 console.log("Hello There!");
-
-//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
--- test.js
+++ test.js
@@ -1,4 +1,3 @@
 "use strict";
 console.log("Hello There!");
-//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
 //# sourceMappingURL=test.js.map
