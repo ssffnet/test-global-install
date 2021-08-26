fb3d3f365fa15757a21935b32c9e565dace4120e
Larry Joy <ljoy@microsoft.com>
2021-08-26 09:43:22 -0700

    super simple

---
+++ test.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+console.log("Hello There!");
+// This works (after a failure)
+// /home/ljoy/.nvm/versions/node/v15.14.0/bin/node /home/ljoy/.nvm/versions/node/v15.14.0/lib/node_modules/npm/bin/npm-cli.js install --force --cache=/home/ljoy/.npm --prefer-offline=false --prefer-online=false --offline=false --no-progress --no-save --no-audit
+//# sourceMappingURL=test.js.map
